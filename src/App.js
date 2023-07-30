023f2f3a11f407bf5230511d5368e818db98be35
O.Korkmaz <h.omr.korkmaz@gmail.com>
2023-07-30 20:29:49 +0200

    set amplify auth and ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Amplify from "aws-amplify"
+import awsconfig from "./aws-exports"
+import {AmplifySignOut, withAuthenticator} from "@aws-amplify/ui-react"
+Amplify.configure(awsconfig)
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+      <AmplifySignOut />
+      <h2>Welcome "User"</h2>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
