a543c2eb0c4e2534c5884511776d104c05f9b9b3
O.Korkmaz <h.omr.korkmaz@gmail.com>
2023-07-30 20:57:51 +0200

    add sign out button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Amplify from "aws-amplify"
-import awsconfig from "./aws-exports"
-import {AmplifySignOut, withAuthenticator} from "@aws-amplify/ui-react"
-Amplify.configure(awsconfig)
-function App() {
+import { Amplify } from 'aws-amplify';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <AmplifySignOut />
-      <h2>Welcome "User"</h2>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default withAuthenticator(App);
